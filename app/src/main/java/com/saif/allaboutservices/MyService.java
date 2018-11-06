ce894504f805a2c03c9a29dc5a7644dc4fc75708
Khaled Saifullah <khaled.saif06@gmail.com>
2018-11-07 01:55:04 +0600

    tested services lifecycle and added a async task to do some task in background thread

--- app/src/main/java/com/saif/allaboutservices/MyService.java
+++ app/src/main/java/com/saif/allaboutservices/MyService.java
@@ -2,6 +2,7 @@ package com.saif.allaboutservices;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -17,7 +18,8 @@ public class MyService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.e(TAG,"onStartCommand: running on "+Thread.currentThread().getName());
-        return super.onStartCommand(intent, flags, startId);
+        new BackgroundTask().execute();
+        return START_STICKY;
 
     }
 
@@ -31,5 +33,37 @@ public class MyService extends Service {
     public void onDestroy() {
         super.onDestroy();
         Log.e(TAG,"onDestroy: running on "+Thread.currentThread().getName());
+    }
+    private class BackgroundTask extends AsyncTask<Void,Void,Void>
+    {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Log.e(TAG,"onPreExecute: running on "+Thread.currentThread().getName());
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Log.e(TAG,"doInBackground: running on "+Thread.currentThread().getName());
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+            Log.e(TAG,"onProgressUpdate: running on "+Thread.currentThread().getName());
+            stopSelf();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Log.e(TAG,"onPostExecute: running on "+Thread.currentThread().getName());
+        }
     }
 }
